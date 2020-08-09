0c54ca1a86f6b0a9e6e9e42bb0cb89a911e94142
dercz <drcz@o2.pl>
2020-08-09 02:17:51 +0200

    wip valuation/model distinction + funny way for dnf conversion

--- formulas-n-valuations.scm
+++ formulas-n-valuations.scm
@@ -1,5 +1,15 @@
 (use-modules (grand scheme))
 
+(define (reduce kons xs) ;; sligthly different from right-fold
+  (match xs
+    ((x x*) (kons x x*))
+    ((x . xs*) (kons x (reduce kons xs*)))))
+
+(e.g. (reduce (lambda (h t) `(& ,h ,t)) '(p q r s))
+      ===> (& p (& q (& r s))))
+
+(e.g. (reduce + '(1 2 3 4)) ===> 10)
+
 
 ;;; "syntax" ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define name? symbol?)
@@ -35,10 +45,10 @@
       ===> (p2 q r p))
 
 
-(define (expressed-as-¬v& form)
-  (match form
+(define (expressed-as-¬v& formula)
+  (match formula
     ('⊥ `(& p (¬ p)))
-    ((? name?) form)
+    ((? name?) formula)
     (('¬ p) `(¬ ,(expressed-as-¬v& p)))
     (('& p q) `(& ,(expressed-as-¬v& p) ,(expressed-as-¬v& q)))
     (('v p q) `(v ,(expressed-as-¬v& p) ,(expressed-as-¬v& q)))
@@ -50,10 +60,10 @@
       ===> (v (v (¬ p) q) (v (& q r) (& (¬ q) (¬ r)))))
 
 
-(define (expressed-as-¬v form)
-  (match form
+(define (expressed-as-¬v formula)
+  (match formula
     ('⊥ `(¬ (v p ('¬ p))))
-    ((? name?) form)
+    ((? name?) formula)
     (('¬ p) `(¬ ,(expressed-as-¬v p)))
     (('& p q) `(¬ (v (¬ ,(expressed-as-¬v p)) (¬ ,(expressed-as-¬v q)))))
     (('v p q) `(v ,(expressed-as-¬v p) ,(expressed-as-¬v q)))
@@ -67,14 +77,14 @@
                     (¬ (v (¬ r) q))))))
 
 
-(define (expressed-as-⊥→ form)
-  (match form
+(define (expressed-as-⊥→ formula)
+  (match formula
     ('⊥ '⊥)
-    ((? name?) form)
+    ((? name?) formula)
     (('¬ p) `(→ ,(expressed-as-⊥→ p) ⊥))
     (('v p q) `(→ (→ ,(expressed-as-⊥→ p) ⊥) ,(expressed-as-⊥→ q)))
     (('→ p q) `(→ ,(expressed-as-⊥→ p) ,(expressed-as-⊥→ q)))
-    (otherwise (expressed-as-⊥→ (expressed-as-¬v form))))) ;; lazy again.
+    (otherwise (expressed-as-⊥→ (expressed-as-¬v formula))))) ;; lazy again.
 
 (e.g. (expressed-as-⊥→ '(v (→ p q) (≡ q r)))
       ===> (→ (→ (→ p q) ⊥)
@@ -108,10 +118,10 @@
             ((p . #f) (q . #f))))
 
 
-(define (value form #;under valuation)
-  (match form
+(define (value formula #;under valuation)
+  (match formula
     ('⊥ #f)
-    ((? name?) (assoc-ref valuation form))
+    ((? name?) (assoc-ref valuation formula))
     (('¬ p) (not (value p valuation)))
     (('& p q) (and (value p valuation) (value q valuation)))
     (('v p q) (or (value p valuation) (value q valuation)))
@@ -121,9 +131,9 @@
 (e.g. (value #;of '(→ (v p (¬ p)) (v q (¬ q))) #;under '((p . #t) (q . #f)))
       ===> #t)
 
-(define (truth-table form)
-  (map (lambda (valuation) `(,valuation ,(value form #;under valuation)))
-         (all-valuations #;over (names #;in form))))
+(define (truth-table formula)
+  (map (lambda (valuation) `(,valuation ,(value formula #;under valuation)))
+         (all-valuations #;over (names #;in formula))))
 
 (e.g. (truth-table '(& p (v q (¬ r))))
  ===> ((((q . #t) (r . #t) (p . #t)) #t)
@@ -136,26 +146,53 @@
        (((q . #f) (r . #f) (p . #f)) #f)))
 
 
-(define (have-same-values-in-all-models? form1 form2)
-  (let* ((names (union (names form1) (names form2)))
-         (truth-values (multicombinations '(#t #f) (length names)))
-         (valuations (map (lambda (tv) (map cons names tv)) truth-values)))
-    (every (lambda (valuation) (equal? (value form1 valuation)
-                                  (value form2 valuation)))
-           valuations)))
+(define (have-same-values-in-all-valuations? formula formula*)
+  (every (lambda (valuation) (equal? (value formula valuation)
+                                (value formula* valuation)))
+         (all-valuations #;over (union (names formula) (names formula*)))))
 
-(e.g. (have-same-values-in-all-models? '(→ p q) '(v (¬ p) q)))
-(e.g. (have-same-values-in-all-models? '(& p q) '(& q p))
+(e.g. (have-same-values-in-all-valuations? '(→ p q) '(v (¬ p) q)))
+(e.g. (have-same-values-in-all-valuations? '(& p q) '(& q p)))
 
 
-(e.g. (have-same-values-in-all-models? 
+(e.g. (have-same-values-in-all-valuations? 
        '(v (→ p q) (≡ q r))
        (expressed-as-¬v& '(v (→ p q) (≡ q r)))))
 
-(e.g. (have-same-values-in-all-models? 
+(e.g. (have-same-values-in-all-valuations? 
        '(v (→ p q) (≡ q r))
        (expressed-as-¬v '(v (→ p q) (≡ q r)))))
 
-(e.g. (have-same-values-in-all-models? 
+(e.g. (have-same-values-in-all-valuations? 
        '(v (→ p q) (≡ q r))
        (expressed-as-⊥→ '(v (→ p q) (≡ q r)))))
+
+
+(define (models #;for formula)
+  (filter (lambda (valuation) (value formula #;under valuation))
+          (all-valuations #;over (names #;in formula))))
+
+(e.g. (models '(& (→ p q) q))
+      ===> (((q . #t) (p . #t))
+            ((q . #t) (p . #f))))
+
+
+;;; "semantic" way of converting into Disjunctive Normal Form:
+
+(define (DNF #;for formula)
+  (define (literal #;from (name . truth-value))
+    (if truth-value name #;else `(¬ ,name)))
+
+  (define (clause #;from valuation)
+    (reduce (lambda (f f*) `(& ,f ,f*)) (map literal valuation)))
+
+  (reduce (lambda (f f*) `(v ,f ,f*)) (map clause (models formula))))
+
+(e.g. (DNF '(→ p q))
+      ===> (v (& q p) (v (& q (¬ p)) (& (¬ q) (¬ p)))))
+
+(e.g. (DNF '(& p (v q (¬ r))))
+      ===> (v (& q (& r p)) (v (& q (& (¬ r) p)) (& (¬ q) (& (¬ r) p)))))
+
+(e.g. (have-same-values-in-all-valuations? '(& p (v q (¬ r)))
+                                           (DNF '(& p (v q (¬ r))))))
